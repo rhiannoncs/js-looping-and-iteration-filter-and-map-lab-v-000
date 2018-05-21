a2487b993ff61195c7f3c8c230e335ab816b5e3a
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-05-21 17:38:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function driversWithRevenueOver(drivers, revenue) {
   return drivers.filter(function(driver) {return driver.revenue > revenue;});
 }
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+  const selectDrivers = driversWithRevenueOver(drivers, revenue);
+  return selectDrivers.map()
+}
